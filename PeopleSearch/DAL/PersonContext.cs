f00625b7a144bdbd53fedaf7198af194327ceb04
Riley <rileydeananderson@gmail.com>
2016-10-15 20:30:44 -0600

    Initial Commit. Added angular. Started work on DB

---
+++ PeopleSearch/DAL/PersonContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PeopleSearch.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace PeopleSearch.DAL
+{
+    public class PersonContext : DbContext
+    {
+
+    }
+}
---
+++ PeopleSearch/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeopleSearch.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+        public string interests { get; set;}
+        public byte[] image { get; set; }
+
+    }
+}
